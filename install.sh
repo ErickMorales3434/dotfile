75cb199f2a2b3837ed62d2b15ad3e0a27e39c269
erick <erickmorales691@gmial.com>
2021-03-30 17:01:11 -0600

    add ascii install

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+function ascii(){
+	echo '██████╗ ███████╗██████╗ ██╗    ██╗███╗   ███╗'
+	echo '██╔══██╗██╔════╝██╔══██╗██║    ██║████╗ ████║'
+	echo '██████╔╝███████╗██████╔╝██║ █╗ ██║██╔████╔██║'
+	echo '██╔══██╗╚════██║██╔═══╝ ██║███╗██║██║╚██╔╝██║'
+	echo '██████╔╝███████║██║     ╚███╔███╔╝██║ ╚═╝ ██║'
+	echo '╚═════╝ ╚══════╝╚═╝      ╚══╝╚══╝ ╚═╝     ╚═╝'
+	echo '												'
+}
+function asciiFinish(){
+	echo '███████╗██╗███╗   ██╗██╗███████╗██╗  ██╗'
+	echo '██╔════╝██║████╗  ██║██║██╔════╝██║  ██║'
+	echo '█████╗  ██║██╔██╗ ██║██║███████╗███████║'
+	echo '██╔══╝  ██║██║╚██╗██║██║╚════██║██╔══██║'
+	echo '██║     ██║██║ ╚████║██║███████║██║  ██║'
+	echo '╚═╝     ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚═╝  ╚═╝'
+	echo '					'
+}
 echo 'Instalacion de BSPWM'
 function InstallYay(){
 	sudo pacman -S git
@@ -11,24 +29,16 @@ function instalador(){
 	sleep 2
 	echo 'Instalando bspwm'
 	sleep 2
-	sudo pacman -S bspwm sxhkd alacritty rofi python-pip feh brightnessctl alsa-utils playerctl
+	sudo pacman -S bspwm sxhkd alacritty rofi python-pip feh brightnessctl alsa-utils playerctl scrot firefox telegram-desktop obs-studio neovim
 }
 function polybar_clearine(){
 	yay -S polybar-git 
 	yay -S clearine-git
-
+	yay -S visual-studio-code-bin
 }
-
-function menu_installer(){
-	#instalador 
-	#polybar_clearine
-	#configurationWorkpaces
-	editBash
-}
-
 function configurationWorkpaces(){
 	echo 'copiando archivos'
-	sleep 3
+	sleep 2
 	mkdir $HOME/.config
 	cp -rv config/bspwm $HOME/.config
 	cp -rv config/sxhkd $HOME/.config
@@ -38,18 +48,36 @@ function configurationWorkpaces(){
 	cp -rv config/picom $HOME/.config
 	cp -rv config/polybar $HOME/.config
 	cp -rv config/rofi $HOME/.config
+	cp -rv config/wal $HOME/.wal
 }
 function installFonts(){
 	echo 'Instalando fuentes'
 	sleep 2
 }
 
-function editBash(){
+function editXinitBash(){
 	touch $HOME/.xinitrc
 	echo 'sxhkd &' >> $HOME/.xinitrc
 	echo 'exec bspwm' >> $HOME/.xinitrc
+	echo 'alias v=nvim' >> $HOME/.bashrc
+	echo 'alias sv='sudo nvim'' >> $HOME/.bashrc
 }
+
+function menu_installer(){
+	echo 'Ingrese el numero de su distribucion actual'
+	read input_distro
+	if [ "$input_distro" = "1" ]; then
+		echo 'distro arch'
+	elif [ "$input_distro" = "2" ]; then
+		echo 'distro debian'
+
+	else
+		echo 'opcion erronea, vuelve a intentarlo' 
+	fi
+}
+ascii
 menu_installer
-bspc wm -r
+asciiFinish
+#bspc wm -r
 
 
