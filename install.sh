a4b665bbfdefeda607f2bae3971e870418e17f4a
erick <erickmorales691@gmial.com>
2021-03-31 20:08:50 -0600

    polybar

--- install.sh
+++ install.sh
@@ -46,8 +46,9 @@ function appsYay(){
 	yay -S visual-studio-code-bin
 }
 function appsGit(){
+	sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev
 	cd $HOME && git clone https://github.com/polybar/polybar.git && cd polybar && ./build.sh
-	sudo apt install python3-pip
+	sudo apt remove i3-wm
 	cd $HOME && git clone https://github.com/yuune/clearine.git && cd clearine && sudo pip install .
 
 }
